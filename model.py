23397c2d3e77d510994b50c467fa2d8fa9bb3723
LIFE1UP <88600101+LIFE1UP@users.noreply.github.com>
2022-04-07 14:04:50 +0900

    Update model.py

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ class NeuralNet(nn.Module):
     def __init__(self, inpt, hidn, oupt):
         super(NeuralNet, self).__init__()
         self.dropout = nn.Dropout(p=0.2)
-        self.sigmoid = nn.Sigmoid()
+        self.act = nn.ReLU()
         
         self.l1 = nn.Linear(inpt, hidn)
         self.l2 = nn.Linear(hidn, hidn)
@@ -14,12 +14,12 @@ class NeuralNet(nn.Module):
 
     def forward(self, x):
         out = self.l1(x)
-        out = self.sigmoid(out)
+        out = self.act(out)
         out = self.l2(out)
-        out = self.sigmoid(out)
+        out = self.act(out)
         out = self.l3(out)
         out = self.dropout(out)
-        out = self.sigmoid(out)
+        out = self.act(out)
         out = self.l4(out)
 
         return out
